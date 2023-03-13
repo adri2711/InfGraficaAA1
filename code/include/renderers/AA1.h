5df1413c450f4edd932f4f8ab7c8ea5f6995acb1
AdriaPerez <adri271101@gmail.com>
2023-03-13 18:01:09 +0100

    loader

--- code/include/renderers/AA1.h
+++ code/include/renderers/AA1.h
@@ -1,8 +1,22 @@
 #pragma once
 #include "Renderer.h"
+#include "Program.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <glm\gtc\type_ptr.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+
 class AA1 :
     public Renderer
 {
-     
-};
+    Program* program;
+    GLuint VAO;
+    GLuint VBO;
 
+public:
+    AA1(int width, int height);
+    ~AA1();
+protected:
+    void render(float dt);
+};
--- code/src/renderers/AA1.cpp
+++ code/src/renderers/AA1.cpp
@@ -1,1 +1,13 @@
 #include "renderers/AA1.h"
+
+AA1::AA1(int width, int height) : Renderer(width, height)
+{
+}
+
+AA1::~AA1()
+{
+}
+
+void AA1::render(float dt)
+{
+}
